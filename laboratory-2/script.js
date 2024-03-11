488a886c070f10cee4cfd28ac947b6173301d14e
denis-vp <pop.denis.v@gmail.com>
2024-03-11 20:08:45 +0200

    Update html structure + document css

--- laboratory-2/script.js
+++ laboratory-2/script.js
@@ -2,7 +2,14 @@ let clock = document.querySelector('#clock');
 
 function updateTime() {
     let hours = new Date().getHours();
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
     let minutes = new Date().getMinutes();
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+
     clock.textContent = hours + ':' + minutes;
 
     setTimeout(updateTime, 1000);
